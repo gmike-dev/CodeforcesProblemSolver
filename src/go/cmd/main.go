95313a96e12acca5d19cdcb48c7f4aee06b7cb7c
coffeek <ya.kofeek@yandex.ru>
2025-09-23 20:47:15 +0400

    Go template

--- src/go/cmd/main.go
+++ src/go/cmd/main.go
@@ -3,27 +3,71 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-var (
-	reader = bufio.NewReader(os.Stdin)
-	writer = bufio.NewWriter(os.Stdout)
-)
-
-func solveCase() {
-
+func (s *solver) solveCase() {
 }
 
-func solve() {
+func (s *solver) solve() {
 	var t int
-	for fmt.Fscan(reader, &t); t >= 0; t-- {
-		solveCase()
+	for s.scanln(&t); t > 0; t-- {
+		s.solveCase()
+	}
+}
+
+func (s *solver) write(a ...any) {
+	fmt.Fprint(s.output, a...)
+}
+
+func (s *solver) writeln(a ...any) {
+	fmt.Fprintln(s.output, a...)
+}
+
+func (s *solver) scan(a ...any) {
+	fmt.Fscan(s.input, a...)
+}
+
+func (s *solver) scanln(a ...any) {
+	fmt.Fscanln(s.input, a...)
+}
+
+func (s *solver) readInt() int {
+	var a int
+	fmt.Fscan(s.input, &a)
+	return a
+}
+
+func (s *solver) read2Int() (int, int) {
+	var a, b int
+	fmt.Fscan(s.input, &a, &b)
+	return a, b
+}
+
+func (s *solver) read3Int() (int, int, int) {
+	var a, b, c int
+	fmt.Fscan(s.input, &a, &b, &c)
+	return a, b, c
+}
+
+type solver struct {
+	input  io.Reader
+	output io.Writer
+}
+
+func newSolver(input io.Reader, output io.Writer) *solver {
+	return &solver{
+		input:  input,
+		output: output,
 	}
 }
 
 func main() {
-	defer writer.Flush()
+	input := bufio.NewReader(os.Stdin)
+	output := bufio.NewWriter(os.Stdout)
+	defer output.Flush()
 
-	solve()
+	s := newSolver(input, output)
+	s.solve()
 }
--- src/go/cmd/main_test.go
+++ src/go/cmd/main_test.go
@@ -1,6 +1,23 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"strings"
+	"testing"
 
-func Test_main(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
+
+func Test1(t *testing.T) {
+	check(t, "", "")
+}
+
+func check(t *testing.T, input string, expected string) {
+	reader := strings.NewReader(input)
+	var buf bytes.Buffer
+	s := newSolver(reader, &buf)
+	s.solve()
+	actual := strings.TrimSpace(buf.String())
+	expected = strings.TrimRight(expected, " \r\n")
+	assert.Equal(t, expected, actual)
 }
